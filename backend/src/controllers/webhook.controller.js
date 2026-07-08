48641f786974631ab9450f14268aedcd774aeeef
adisamuiz <107328148+adisamuiz@users.noreply.github.com>
2026-07-08 01:13:33 +0100

    feat: implement webhook handling for Nomba payments and update routes

--- backend/src/controllers/webhook.controller.js
+++ backend/src/controllers/webhook.controller.js
@@ -3,23 +3,22 @@ import crypto from 'crypto'
 
 const verifyAndReceiveWebhook = async (req, res) => {
     try{
-        // const nombaSignature = req.header['nomba-signature'];
-        // if (!nombaSignature) {
-        //     return res.status(401).json({ message: 'Missing signature' });
-        // }
-
-        // const payloadString = JSON.stringify(req.body);
-        // const expectedSignature = crypto
-        //     .createHmac('sha256', config.NOMBA_WEBHOOK_SIGNATURE)
-        //     .update(payloadString)
-        //     .digest('base64');
-
-        // if (expectedSignature !== nombaSignature) {
-        //     return res.status(401).json({ message: 'Unauthorized payload' });
-        // }
-        if (req.body) {
-            res.status(200).send("success");
+        const nombaSignature = req.header['nomba-signature'];
+        if (!nombaSignature) {
+            return res.status(401).json({ message: 'Missing signature' });
         }
+
+        const payloadString = JSON.stringify(req.body);
+        const expectedSignature = crypto
+            .createHmac('sha256', config.NOMBA_WEBHOOK_SIGNATURE)
+            .update(payloadString)
+            .digest('base64');
+
+        if (expectedSignature !== nombaSignature) {
+            return res.status(401).json({ message: 'Unauthorized payload' });
+        }
+        
+        console.log(res.status(200).json(req.body));
     } catch {
         res.status(500).json({ message: 'Coould not resolve web hook' });
     }
